397549e2043e97ae04f06159ade6589c3545785d
Mario Kratafila <kratafila@gmail.com>
2026-02-07 10:26:25 +0100

    Application Insights

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -4,8 +4,63 @@ import random
 import redis
 import socket
 import sys
+import logging
+from datetime import datetime
+
+# App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+
+# For metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
+
+#Logging
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
+# Custom Logging
+logger = logging.getLogger(__name__)
+handler = AzureLogHandler(connection_string='InstrumentationKey=[7fb9c635-d1e6-455a-97a9-ace50168a6fa]')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(handler)
+# Logging custom Events 
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=[7fb9c635-d1e6-455a-97a9-ace50168a6fa]'))
+# Set the logging level
+logger.setLevel(logging.INFO)
+
+
+# Metrics
+exporter = metrics_exporter.new_metrics_exporter(
+enable_standard_metrics=True,
+connection_string='InstrumentationKey=[7fb9c635-d1e6-455a-97a9-ace50168a6fa]')
+view_manager.register_exporter(exporter)
+
+# Tracing
+tracer = Tracer(
+exporter=AzureExporter(
+  connection_string='InstrumentationKey=[7fb9c635-d1e6-455a-97a9-ace50168a6fa]'),
+sampler=ProbabilitySampler(1.0),
+)
 
 app = Flask(__name__)
+
+# Requests
+middleware = FlaskMiddleware(
+app,
+exporter=AzureExporter(connection_string="InstrumentationKey=[7fb9c635-d1e6-455a-97a9-ace50168a6fa]"),
+sampler=ProbabilitySampler(rate=1.0)
+)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -55,7 +110,12 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
+        with tracer.span(name="Cats Vote") as span:
+            print("Cats Vote")
+        
         vote2 = r.get(button2).decode('utf-8')
+        with tracer.span(name="Dogs Vote") as span:
+            print("Dogs Vote")    
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -68,7 +128,13 @@ def index():
             r.set(button1,0)
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.info('Cats Vote', extra=properties)
+
             vote2 = r.get(button2).decode('utf-8')
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.info('Dogs Vote', extra=properties)
+
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
         
         else:
@@ -79,7 +145,12 @@ def index():
             
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.info('Cats Vote', extra=properties)
+
             vote2 = r.get(button2).decode('utf-8')  
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.info('Dogs Vote', extra=properties)  
                 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
